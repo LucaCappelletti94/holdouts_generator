0a01f073e03efb0a1d85b042c57623c341957ded
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-06-02 21:58:12 +0200

    Refactore holdouts generator

--- holdouts_generator/holdouts_generator.py
+++ holdouts_generator/holdouts_generator.py
@@ -4,19 +4,6 @@ import shutil
 from auto_tqdm import tqdm
 from typing import List
 
-
-def load_cache(dataset, holdout, name, cache_dir:str):
-    """Return given holdout, also handles cache if it is enabled."""
-    path = "{cache_dir}/{name}.pickle".format(cache_dir=cache_dir, name=name)
-    if os.path.exists(path):
-        with open(path, "rb") as f:
-            return pickle.load(f)
-    os.makedirs(cache_dir, exist_ok=True)
-    data = holdout(dataset)
-    with open(path, "wb") as f:
-        pickle.dump(data, f)
-    return data
-
 def get_desc(level:int):
     if level==0:
         return "Holdouts"
@@ -26,7 +13,7 @@ def get_desc(level:int):
         return "Inner holdouts (level {level})".format(level=level)
 
 
-def holdouts_generator(*dataset, holdouts:List, verbose:bool=True, cache:bool=False, cache_dir:str=".holdouts", level:int=0):
+def holdouts_generator(*dataset:List, holdouts:List, verbose:bool=True, cache:bool=False, cache_dir:str=".holdouts", level:int=0):
     """Return validation dataset and another holdout generator
         dataset, iterable of datasets to generate holdouts from.
         holdouts:List, list of holdouts callbacks.
@@ -34,24 +21,30 @@ def holdouts_generator(*dataset, holdouts:List, verbose:bool=True, cache:bool=Fa
         cache:bool=False, whetever to cache or not the rendered holdouts.
         cache_dir:str=".cache", directory where to cache the holdouts.
     """
+    if holdouts is None:
+        return None
     def generator():
         for outer_holdout, name, inner_holdouts in tqdm(list(holdouts), verbose=verbose, desc=get_desc(level)):
-            validation = load_cache(dataset, outer_holdout, name, cache_dir) if cache else outer_holdout(dataset)
+            path = "{cache_dir}/{name}".format(cache_dir=cache_dir, name=name)
+            pickle_path = "{path}.pickle".format(path=path)
+            if cache and os.path.exists(pickle_path):
+                with open(pickle_path, "rb") as f:
+                    validation = pickle.load(f)
+            validation = outer_holdout(dataset)
+            if cache:
+                os.makedirs(cache_dir, exist_ok=True)
+                with open(pickle_path, "wb") as f:
+                    pickle.dump(validation, f)
+
             training, testing = validation[::2], validation[1::2]
-            if inner_holdouts is None:
-                yield (training, testing), None
-            else:
-                yield (training, testing), holdouts_generator(
-                    *training,
-                    holdouts=inner_holdouts,
-                    verbose=verbose,
-                    cache=cache,
-                    cache_dir="{cache_dir}/{name}".format(
-                        cache_dir=cache_dir,
-                        name=name
-                    ),
-                    level=level+1
-                )
+            yield (training, testing), holdouts_generator(
+                *training,
+                holdouts=inner_holdouts,
+                verbose=verbose,
+                cache=cache,
+                cache_dir=path,
+                level=level+1
+            )
     return generator
 
 
