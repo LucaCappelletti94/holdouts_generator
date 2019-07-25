fc1e759824d6013ef18dc3270331cf6f91b22d2f
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-07-25 19:57:38 +0200

    extended tests

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -2,8 +2,8 @@ from holdouts_generator import random_holdouts, chromosomal_holdouts
 from random import randint
 
 example_random_holdouts = random_holdouts(
-    [0.1, 0.2],
-    [2, 1]
+    [0.1, 0.2, 0.3],
+    [2, 1, 1]
 )
 
 example_chromosomal_holdouts = chromosomal_holdouts([
