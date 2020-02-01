43056a33ad56acc64f76703db7a71510288bd4ea
Luca Cappelletti <cappelletti.luca94@gmail.com>
2020-02-01 11:52:46 +0100

    Resolved code smells

--- holdouts_generator/utils/metrics.py
+++ holdouts_generator/utils/metrics.py
@@ -3,18 +3,10 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import balanced_accuracy_score
 from sklearn.metrics import average_precision_score
-from sklearn.metrics import cohen_kappa_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
-from sklearn.metrics import hamming_loss
-from sklearn.metrics import hinge_loss
-from sklearn.metrics import jaccard_score
-from sklearn.metrics import log_loss
-from sklearn.metrics import matthews_corrcoef
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
-from sklearn.metrics import zero_one_loss
-from sklearn.metrics import brier_score_loss
 from sklearn.metrics import roc_auc_score
 from deflate_dict import deflate
 
